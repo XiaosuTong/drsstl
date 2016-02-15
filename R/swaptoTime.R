cae2813b5b6cf374c1e535fa3284c61208957a73
XiaosuTong <xiaosutong@gmail.com>
2016-02-15 17:30:37 -0500

    inital

---
+++ R/swaptoTime.R
@@ -0,0 +1,80 @@
+#' Spatially Local Polynomial Regression Fitting
+#'
+#' The first layer of the Spatial locally weighted regression, using local fitting with different 
+#' type of distance calculation.
+#'
+#' @param input
+#'     The path of input sequence file on HDFS. It should be by location division.
+#' @param output
+#'     The path of output sequence file on HDFS. It is by time division.
+#' @param reduceTask
+#'     The number of the reduce tasks.
+#' @param elevFlag
+#'     Logical argument, if TRUE, then the elevation attribute from input value data.frame is log2 transformation
+#'     If FALSE, a log2 transformation is added to the elevation attribute.
+#' @details
+#'     swaptoTime is used for switch division by location to division by time.
+#' @author 
+#'     Xiaosu Tong 
+#' @export
+#' @examples
+#'     FileInput <- "/ln/tongx/Spatial/a1950/bystation"
+#'     FileOutput <- "/ln/tongx/Spatial/a1950/bymonth"
+#'     \dontrun{
+#'       swaptoTime(FileInput, FileOutput, elevFlag=TRUE)
+#'     }
+
+swaptoTime <- function(input, output, elevFlag=TRUE, reduceTask) {
+
+  job <- list()
+  job$map <- expression({
+    lapply(seq_along(map.values), function(r) {
+      d_ply(
+        .data = map.values[[r]],
+        .variable = c("year","month"),
+        .fun = function(k, station = map.keys[[r]]) {
+          key <- c(unique(k$year), unique(as.character(k$month)))
+          value <- subset(k, select = -c(year, month))
+          value$station.id <- station
+          value$lon <- as.numeric(attributes(map.values[[r]])$loc[1])
+          value$lat <- as.numeric(attributes(map.values[[r]])$loc[2])
+          if (!elevFlag) {
+            value$elev2 <- as.numeric(attributes(map.values[[r]])$loc[3])
+          } else {
+          	value$elev2 <- log2(as.numeric(attributes(map.values[[r]])$loc[3]) + 128)
+          }
+          rhcollect(key, value)
+      })
+    })
+  })
+  job$reduce <- expression(
+    pre = {
+      combine <- data.frame()
+    },
+    reduce = {
+      combine <- rbind(combine, do.call(rbind, reduce.values))
+    },
+    post = {
+      rhcollect(reduce.key, combine)
+    }
+  )
+  job$setup <- expression(
+    map = {library(dplyr, lib.loc=lib.loc)}
+  )
+  job$mapred <- list(
+    mapred.reduce.tasks = reduceTask,  #cdh3,4
+    mapreduce.job.reduces = reduceTask,  #cdh5
+    mapred.tasktimeout = 0,
+    rhipe_reduce_buff_size = 10000
+  )
+  job$combiner <- TRUE
+  job$input <- rhfmt(input, type="sequence")
+  job$output <- rhfmt(output, type="sequence")
+  job$mon.sec <- 20
+  job$jobname <- output
+  job$readback <- FALSE  
+
+  job.mr <- do.call("rhwatch", job)  
+
+}
+
