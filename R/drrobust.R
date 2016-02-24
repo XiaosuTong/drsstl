d6a3ca98f2d66b0e783dabd46909b3217ffdc9fb
XiaosuTong <xiaosutong@gmail.com>
2016-02-24 13:40:02 -0500

    add drrobust

--- R/drrobust.R
+++ R/drrobust.R
@@ -2,9 +2,9 @@
 #'
 #' Calculate the robust weight for each observation of the time series
 #'
-#' @param n
+#' @param input
 #'     The path of input sequence file on HDFS. It should be by location division.
-#' @param Y
+#' @param output
 #'     The path of output sequence file on HDFS. It is by time division.
 #' @param seasonal
 #'     The number of the reduce tasks.
@@ -19,21 +19,40 @@
 #'     Xiaosu Tong 
 #' @export
 
-drrobust <- function(n, Y, seasonal, trend, zero.weight) {
+drrobust <- function(input, output, vari, zero.weight) {
 
-  mid1 <- floor(n / 2 + 1)
-  mid2 <- n - mid1 + 1
-  R <- Y - seasonal - trend
-  R.abs <- abs(R)
-  h <- 3 * sum(sort(R.abs)[mid1:mid2])
-  h9 <- 0.999 * h
-  h1 <- 0.001 * h
-  w <- (1 - (R.abs / h)^2)^2
-  w[R.abs <= h1] <- 1
-  w[R.abs >= h9] <- 0
-  w[w == 0] <- zero.weight
-  w[is.na(w)] <- 1
-
-  return(w)
+  jobW <- list()
+  jobW$map <- expression({
+    lapply(seq_along(map.keys), function(r) {
+      n <- nrow(map.values[[r]])
+      mid1 <- floor(n / 2 + 1)
+      mid2 <- n - mid1 + 1
+      R.abs <- abs(map.values[[r]][, vari] - map.values[[r]]$trend - map.values[[r]]$seasonal)
+      h <- 3 * sum(sort(R.abs)[mid1:mid2])
+      h9 <- 0.999 * h
+      h1 <- 0.001 * h
+      w <- (1 - (R.abs / h)^2)^2
+      w[R.abs <= h1] <- 1
+      w[R.abs >= h9] <- 0
+      w[w == 0] <- zero.weight
+      w[is.na(w)] <- 1
+      map.values[[r]]$weight <- w
+      rhcollect(map.keys[[r]], map.values[[r]])
+    })
+  })
+  jobW$parameters <- list(
+    vari = vari, zero.weight = zero.weight
+  )
+  jobW$input <- rhfmt(input, type = "sequence")
+  jobW$output <- rhfmt(output, type = "sequence")
+  jobW$mapred <- list(
+    mapred.reduce.tasks = reduceTask,  #cdh3,4
+    mapreduce.job.reduces = reduceTask,  #cdh5
+    io.sort.mb = io.sort,
+    io.sort.spill.percent = spill.percent 
+  )
+  jobW$readback <- FALSE
+  jobW$jobname <- output
+  job.mr <- do.call("rhwatch", jobW)
 
 }
