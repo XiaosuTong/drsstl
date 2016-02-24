2a4d371a9471a871c18790bbeee4f9ec43e2bbef
XiaosuTong <xiaosutong@gmail.com>
2016-02-23 23:44:22 -0500

    add t.window

--- R/drinner.R
+++ R/drinner.R
@@ -41,7 +41,7 @@
 #'     FileInput <- "/ln/tongx/Spatial/tmp/tmax/a1950/byseason"
 #'     FileOutput <- "/ln/tongx/Spatial/tmp/tmax/a1950/byseason.season"
 #'     \dontrun{
-#'       drinner(input=FileInput, output=FileOutput, vari="resp", cyctime="year", seaname = "month", n=576, n.p=12, s.window=13, s.degree=1, reduceTask=10, spill.percent = 0.85, io.sort = 256, control=spacetime.control(libLoc=.libPaths()))
+#'       drinner(input=FileInput, output=FileOutput, vari="resp", cyctime="year", seaname = "month", n=576, n.p=12, s.window=13, s.degree=1, t.window = 241, t.degree = 1, reduceTask=10, spill.percent = 0.85, io.sort = 256, control=spacetime.control(libLoc=.libPaths()))
 #'     }
 #' @importFrom stats frequency loess median predict quantile weighted.mean time
 #' @importFrom utils head stack tail
@@ -72,6 +72,39 @@ drinner <- function(input, output, infill = TRUE, vari, cyctime, seaname, n, n.p
     if(! all(x == 0 | x == 1 | x == 2)) stop("Smoothing degree must be 0, 1, or 2")
   }
 
+  get.t.window <- function(t.dg, s.dg, n.s, n.p, omega) {
+    if(t.dg == 0) t.dg <- 1
+    if(s.dg == 0) s.dg <- 1
+    
+    coefs_a <- data.frame(
+      a = c(0.000103350651767650, 3.81086166990428e-6),
+      b = c(-0.000216653946625270, 0.000708495976681902))
+    coefs_b <- data.frame(
+      a = c(1.42686036792937, 2.24089552678906),
+      b = c(-3.1503819836694, -3.30435316073732),
+      c = c(5.07481807116087, 5.08099438760489))
+    coefs_c <- data.frame(
+      a = c(1.66534145060448, 2.33114333880815),
+      b = c(-3.87719398039131, -1.8314816166323),
+      c = c(6.46952900183769, 1.85431548427732))
+    
+    # estimate critical frequency for seasonal
+    betac0 <- coefs_a$a[s.dg] + coefs_a$b[s.dg] * omega
+    betac1 <- coefs_b$a[s.dg] + coefs_b$b[s.dg] * omega + coefs_b$c[s.dg] * omega^2
+    betac2 <- coefs_c$a[s.dg] + coefs_c$b[s.dg] * omega + coefs_c$c[s.dg] * omega^2
+    f_c <- (1 - (betac0 + betac1 / n.s + betac2 / n.s^2)) / n.p
+    
+    # choose
+    betat0 <- coefs_a$a[t.dg] + coefs_a$b[t.dg] * omega
+    betat1 <- coefs_b$a[t.dg] + coefs_b$b[t.dg] * omega + coefs_b$c[t.dg] * omega^2
+    betat2 <- coefs_c$a[t.dg] + coefs_c$b[t.dg] * omega + coefs_c$c[t.dg] * omega^2
+    
+    betat00 <- betat0 - f_c
+    
+    n.t <- nextodd((-betat1 - sqrt(betat1^2 - 4 * betat00 * betat2)) / (2 * betat00))
+    n.t
+  }
+
   periodic <- FALSE
   if (is.character(s.window)) {
     if (is.na(pmatch(s.window, "periodic")))
@@ -92,11 +125,19 @@ drinner <- function(input, output, infill = TRUE, vari, cyctime, seaname, n, n.p
     l.window <- wincheck(l.window)
   }
 
+  if (is.null(t.window)) {
+    t.window <- get.t.window(t.degree, s.degree, s.window, n.p, critfreq)
+  } else {
+    t.window <- wincheck(t.window)
+  }
+
   degcheck(s.degree)
   degcheck(l.degree)
+  degcheck(t.degree)
 
   if(is.null(s.jump) || length(s.jump)==0) s.jump <- ceiling(s.window / 10)
   if(is.null(l.jump) || length(l.jump)==0) l.jump <- ceiling(l.window / 10)
+  if(is.null(t.jump) || length(t.jump)==0) t.jump <- ceiling(t.window / 10)
   # start and end indices for after adding in extra n.p before and after 
   st <- n.p + 1
   nd <- n + n.p
