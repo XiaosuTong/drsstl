598d41c30b24e94ddf9014871efff76fb9653ee2
XiaosuTong <xiaosutong@gmail.com>
2016-03-12 15:26:23 -0500

    add read buffer

--- R/mapreduce.control.R
+++ R/mapreduce.control.R
@@ -35,7 +35,7 @@ mapreduce.control <- function(
   spill_percent = 0.8, io_sort = 100, task_io_sort_factor = 10,
   reduce_parallelcopies = 5, reduce_shuffle_input_buffer_percent = 0.70,
   reduce_shuffle_merge_percent = 0.66, reduce_merge_inmem = 1000,
-  reduce_input_buffer_percent = 0, reduce_buff_size = 10000) {
+  reduce_input_buffer_percent = 0, reduce_buff_read = 150, map_buffer_read = 150) {
   
   list(
     reduceTask = reduceTask, libLoc=libLoc,
@@ -52,7 +52,8 @@ mapreduce.control <- function(
     # mapreduce.reduce.input.buffer.percent
     reduce_input_buffer_percent = reduce_input_buffer_percent,
     # Rhipe argument
-    reduce_buff_size = reduce_buff_size,
+    reduce_buff_read = reduce_buff_read*2^20,
+    map_buffer_read map_buffer_read*2^20
     BLK = BLK*2^20
   )
 
