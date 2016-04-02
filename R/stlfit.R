7d964fc7bcb1a581bc26b2c507a4d7b21dfcd66c
XiaosuTong <xiaosutong@gmail.com>
2016-04-02 10:10:24 -0400

    remove date from the output df

--- R/stlfit.R
+++ R/stlfit.R
@@ -39,7 +39,7 @@ stlfit <- function(input, output, model_control=spacetime.control(), cluster_con
       # value originally is a data.frame with 4 columns, vectorize it 
       #rhcollect(map.keys[[r]], unname(unlist(cbind(value, subset(fit, select = c(seasonal, trend))))))
       names(value) <- c(Mlcontrol$time, Mlcontrol$vari)
-      value <- cbind(value, subset(fit, select = c(seasonal, trend)))
+      value <- cbind(subset(value, select = -c(date)), subset(fit, select = c(seasonal, trend)))
       rownames(value) <- NULL
       rhcollect(map.keys[[r]], value)
     })
@@ -77,17 +77,17 @@ stlfit <- function(input, output, model_control=spacetime.control(), cluster_con
 }
 
 
-#FileInput <- "/wsc/tongx/spatem/tmax/sim/bystat128"
-#FileOutput <- "/wsc/tongx/spatem/tmax/sim/bystatfit128"#
-#
-#me <- mapreduce.control(
-#  libLoc=lib.loc, reduceTask=0, BLK=128, 
-#  map_jvm = "-Xmx4096m", map_memory = 5120,
-#  map_buffer_read = 100, map_buffer_size = 10000
-#)
-#you <- spacetime.control(
-#  vari="resp", time="date", seaname="month", 
-#  n=786432, n.p=12, s.window=13, t.window = 241, 
-#  degree=2, span=0.015, Edeg=2
-#)
-#stlfit(FileInput, FileOutput, model_control=you, cluster_control=me)
+ FileInput <- "/wsc/tongx/spatem/tmax/sim/bystat128"
+ FileOutput <- "/wsc/tongx/spatem/tmax/sim/bystatfit128"
+ 
+ me <- mapreduce.control(
+   libLoc=lib.loc, reduceTask=0, BLK=128, 
+   map_jvm = "-Xmx4096m", map_memory = 5120,
+   map_buffer_read = 100, map_buffer_size = 10000
+ )
+ you <- spacetime.control(
+   vari="resp", time="date", seaname="month", 
+   n=786432, n.p=12, s.window=13, t.window = 241, 
+   degree=2, span=0.015, Edeg=2
+ )
+ stlfit(FileInput, FileOutput, model_control=you, cluster_control=me)
