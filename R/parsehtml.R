e42c767a70cf51b3c699ec34e274d5e18c181b55
XiaosuTong <xiaosutong@gmail.com>
2016-04-23 00:17:02 -0400

    add func to parse and get spill info

---
+++ R/parsehtml.R
@@ -0,0 +1,39 @@
+#' @export
+
+parsehtml <- function(jobid, type="map") {
+
+  theurl <- paste("http://wsc-adm.rcac.purdue.edu:19888/jobhistory/jobcounters/", jobid, sep="")
+  
+  a <- html_nodes(read_html(theurl), "tr")
+  L <- length(a)
+
+  if (type == "map") {
+
+    for (i in 1:L) {
+      r <- html_nodes(a[i], "td")
+      target <- as.character(html_attrs(r)[1])
+      if( target == "SPILLED_RECORDS") {
+        spill <- as.numeric(strsplit(html_text(r[2]), "[ \n]+")[[1]][2])
+      }
+      if( target == "MAP_OUTPUT_RECORDS") {
+        output <- as.numeric(strsplit(html_text(r[2]), "[ \n]+")[[1]][2])
+      }
+    }
+    return(spill/output)
+
+  } else {
+
+    for (i in 1:L) {
+      r <- html_nodes(a[i], "td")
+      target <- as.character(html_attrs(r)[1])
+      if( target == "SPILLED_RECORDS") {
+        spill <- as.numeric(strsplit(html_text(r[3]), "[ \n]+")[[1]][2])
+      }
+      if( target == "REDUCE_INPUT_RECORDS") {
+        output <- as.numeric(strsplit(html_text(r[3]), "[ \n]+")[[1]][2])
+      }
+    }
+    return(spill/output)
+    
+  }
+}
