0034feb93b2889907c3ed6cc2ea63dea2ff06b4e
Barret Schloerke <schloerke@gmail.com>
2016-06-06 22:50:15 -0400

    add local imports and data descriptions

---
+++ R/drSpaceTime-package.R
@@ -0,0 +1,32 @@
+#' @importFrom stats as.formula loess.control
+NULL
+
+
+
+#' DATA TITLE
+#'
+#' @name station_info
+#' @docType data
+#' @description
+#' DESCRIPTION
+#'
+#' @source
+#' 0-5 years: \url{http://www.who.int/childgrowth/software/en/}
+#'
+#' 5-15 years: \url{http://www.who.int/growthref/tools/en/}
+#' @keywords data
+NULL
+
+#' DATA TITLE
+#'
+#' @name tmax_all
+#' @docType data
+#' @description
+#' DESCRIPTION
+#'
+#' @source
+#' 0-5 years: \url{http://www.who.int/childgrowth/software/en/}
+#'
+#' 5-15 years: \url{http://www.who.int/growthref/tools/en/}
+#' @keywords data
+NULL
